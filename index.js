519a96eef9ca58f480000389d6a3708a08bdd4a4
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-05-09 18:08:17 -0400

    controller and index are set up

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const villains = require('./villains')
-const {getVillains} = require('./controller/villains.js')
+const { getVillains } = require('./controller/villains.js')
 const app = express()
 app.get('/', getVillains)
-app.all('*',)
+app.get('/villains/:slug',)
 app.listen(2020, () => {
     console.log('hi')
 })
