76f8f949c17f3b36440d531e28411521377d8878
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-05-09 23:32:05 -0400

    will need to review some material, having a hard to time catching to this. I will try to get this submmited before class monday

--- controller/villains.js
+++ controller/villains.js
@@ -1,10 +1,14 @@
 const villains = require('../villains')
-
-const getVillains = (req,res) => {
+const getVillains = async(req,res) => {
     return res.send(villains)
 }
 const noEnter = (req,res) => {
     return res.Status(404)
 }
+const slugger = (req,res) =>{
+    const { slug } = req.params
+    const villainMatch = await mo
+    
+}
 
-module.exports = {getVillains}
+module.exports = { getVillains }
