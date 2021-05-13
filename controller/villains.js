178c9e667cd704787a94786eebf35173d002e398
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-05-12 21:43:43 -0400

    my addNewTeam function is dont but I an error when i try to post

--- controller/villains.js
+++ controller/villains.js
@@ -1,8 +1,8 @@
 const models = require('../models')
-const getVillains = async (request, response) => {
+const getVillains = async (req, res) => {
   const villains = await models.villains.findAll({ attributes: ['name', 'movie', 'slug'] })
 
-  return response.send(villains)
+  return res.send(villains)
 }
 const slugger = async (req, res) => {
   const { slug } = req.params
@@ -10,6 +10,18 @@ const slugger = async (req, res) => {
 
   return villainMatch ? res.send(villainMatch) : res.sendStatus(404)
 }
+const addNewTeam = async (req, res) => {
+  const { name, movie, slug } = req.body
+
+  if (!name || !movie || !slug) {
+    return res.status(400).send('You have not presented the attributes required: name, movie, slug')
+  }
+
+  const newVillain = await models.villains.create({ name, movie, slug })
+
+  return res.status(201).send(newVillain)
+}
 
 
-module.exports = { getVillains, slugger }
+
+module.exports = { getVillains, slugger, addNewTeam }
