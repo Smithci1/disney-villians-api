d014bbca42ccd55529d54c8b0de819e67e1f1bc8
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-05-13 15:03:43 -0400

    controller

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const villains = require('./villains')
-const { getVillains } = require('./controller/villains.js')
+const { getVillains, slugger, addNewVillain } = require('./controller/villains.js')
 const app = express()
-app.get('/', getVillains)
-app.get('/villains/:slug',)
+
+app.get('/villains', getVillains)
+app.get('/villains/:slug', slugger)
+app.post('/villains', express.json(), addNewVillain)
 app.listen(2020, () => {
-    console.log('hi')
+  console.log('hi')
 })
