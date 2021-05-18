d52c6fe4de95dd9aca8a5e754db2741bbf661283
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-05-18 16:01:19 -0400

    skeleton for controller test have been created

---
+++ tests/controller/villains.test.js
@@ -0,0 +1,16 @@
+const chai = require('chai')
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+const { describe, it } = require('sinon-chai')
+const { getVillains, slugger, addNewVillain } = require('../../contollers/villains')
+
+chai.use(sinonChai)
+const { expect } = chai
+
+describe ('Controllers - villains', () => {
+    describe('getVillains' () => {})
+
+    describe('slugger' () => {})
+
+    describe('addNewVillain' () => {})
+})
