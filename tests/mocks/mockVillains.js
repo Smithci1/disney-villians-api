963c6692539f7822beebf5334f3128d36011a6c8
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-05-18 15:39:32 -0400

    mock villains added to the mock folder

---
+++ tests/mocks/mockVillains.js
@@ -0,0 +1,23 @@
+const mockVillains = [{
+  'name': 'Ursula',
+  'movie': 'The Little Mermaid',
+  'slug': 'ursula'
+},
+{
+  'name': 'Sir Hiss',
+  'movie': 'Robin Hood',
+  'slug': 'sir-hiss'
+},
+{
+  'name': 'Red Skull',
+  'movie': 'Captain America: The First Avenger',
+  'slug': 'red-skull'
+}]
+
+const sMockVillain = [{
+  'name': 'Shere Khan',
+  'movie': 'The Jungle Book',
+  'slug': 'shere-khan'
+}]
+
+module.exports = { mockVillains, sMockVillain }
