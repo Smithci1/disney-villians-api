0e2586d9ff2d51b3daafc17a9e79def3a515fbc8
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-05-18 16:23:20 -0400

    describe for first test is now completed

--- tests/controller/villains.test.js
+++ tests/controller/villains.test.js
@@ -3,14 +3,23 @@ const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
 const { describe, it } = require('sinon-chai')
 const { getVillains, slugger, addNewVillain } = require('../../contollers/villains')
+const { mockVillains, sMockVillain } = require('../mocks/mockVillains')
 
 chai.use(sinonChai)
 const { expect } = chai
 
-describe ('Controllers - villains', () => {
-    describe('getVillains' () => {})
+describe('Controllers - villains', () => {
+  describe('getVillains', () => {
+    it('retrieves a list of villains from the database and calls res.send() with the list', async () => {
+      const stubbedFindAll = sinon.stub(models.villains, 'findAll').returns(mockVillains)
+      const stubbedSend = sinon.stub()
+      const res = { send: stubbedSend }
 
-    describe('slugger' () => {})
+      await getVillains({}, res)
+    })
+  })
 
-    describe('addNewVillain' () => {})
+  describe('slugger', () => {})
+
+  describe('addNewVillain', () => {})
 })
