27a24f529e0a8bc4a1ab510a9593fc97828f3764
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-05-19 17:38:27 -0400

    did created my slugger and addNewvillain tests but they are not passsing, they both have the same error

--- tests/controller/villains.test.js
+++ tests/controller/villains.test.js
@@ -4,17 +4,22 @@ const sinonChai = require('sinon-chai')
 
 const { describe, it } = require('mocha')
 const models = require('../../models')
+const { mockVillains, singleMockvillain } = require('../mocks/mockData')
 const { getVillains, slugger, addNewVillain } = require('../../controller/villains')
-const { mockVillains, singleMockvillain } = require('../mocks/mockVillains')
 
 chai.use(sinonChai)
 const { expect } = chai
 
 describe('Controllers - villains', () => {
-  let stubbedFindOne
   let res
+  let sandbox
+  let stubbedCreate
+  let stubbedFindOne
+  let stubbedFindAll
   let stubbedSend
-
+  let stubbedSendStatus
+  let stubbedStatus
+  let stubbedStatusDotSend
 
 
   describe('getVillains', () => {
@@ -29,25 +34,48 @@ describe('Controllers - villains', () => {
         expect(stubbedFindAll).to.have.callCount(1)
         expect(stubbedSend).to.have.been.calledWith(mockVillains)
       })
+  })
 
 
-    describe('slugger', () => {
-      it('retrieves the villain associated with the provided slug from the database and calls res.send() ',
-        async () => { stubbedFindOne.returns(singleMockvillain)
+  describe('slugger', () => {
+    it('retrieves the villain associated with the provided slug from the database and calls it with res.send() ',
+      async () => {
+        stubbedFindOne.returns(singleMockvillain)
+        const stubbedSend = sinon.stub()
+        const res = { send: stubbedSend }
 
+        const req = { params: { slug: 'Shere khan' } }
 
-          const req = { params: { slug: 'shere-khan' } }
+        await slugger(req, res)
 
-
-          await slugger(req, res)
-
-          expect(stubbedFindOne).to.have.been.calledWith({
-            where: { slug: 'shere-khan' },
-            attributes: ['name', 'movie', 'slug']
-          })
-          expect(stubbedSend).to.have.been.calledWith(singleMockvillain)
+        expect(stubbedFindOne).to.have.been.calledWith({
+          where: { slug: 'Shere khan' },
+          attributes: ['name', 'movie', 'slug']
         })
-    })
-    describe('addNewVillain', () => {})
+        expect(stubbedSend).to.have.been.calledWith(singleMockvillain)
+      })
+  })
+  describe('addNewVillain', () => {
+    it('creates a new villain database item from the data provided and responds with a 200 status and the new item',
+      async () => {
+        stubbedCreate.returns(singleMockvillain)
+        const req = {
+          body: {
+            name: 'Shere Khan',
+            movie: 'The Jungle Book',
+            slug: 'shere-khan',
+          }
+        }
+
+        await addNewVillain(req, res)
+
+        expect(stubbedCreate).to.have.been.calledWith({
+          name: 'Shere Khan',
+          movie: 'The Jungle Book',
+          slug: 'shere-khan',
+        })
+        expect(stubbedStatus).to.have.been.calledWith(201)
+        expect(stubbedStatusDotSend).to.have.been.calledWith(singleMockvillain)
+      })
   })
 })
